cbb950350e11ace08b2cc53f3e3fafe933d5646b
shobhnik13 <shobhnikw@gmail.com>
2022-12-27 23:04:49 +0530

    project initialised

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-2xl text-red-900'>hey</h1>
+      </div>
+  )
+}
+
+export default App
