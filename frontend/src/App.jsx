94fe309cd43e33c86c569042fac5d3df3f5505d1
shobhnik13 <shobhnikw@gmail.com>
2022-12-29 02:06:46 +0530

    added create todo function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './components/Todo';
 import {db} from './firebase'
-import {query,collection, onSnapshot, updateDoc, doc, deleteDoc} from 'firebase/firestore'
+import {query,collection, onSnapshot, updateDoc, doc, deleteDoc, addDoc} from 'firebase/firestore'
 function App() {
   //creating a new state for changing todos named todos
   const [todos,setTodos]=useState([])
@@ -33,16 +33,27 @@ await updateDoc(doc(db,'todos',Todo.id),{
 const deleteTodo=async (dummyTodo)=>{
   await deleteDoc(doc(db,'todos',dummyTodo.id))
 }
-
-
-
-
+//add todo
+//creating a state for the text in input 
+const [inp,setInp]=useState('');
+//preventing the reload when we add input/todo
+const addTodo=async (e)=>{
+e.preventDefault()
+if(inp===''){
+  alert('Please enter a valid todo')
+}
+addDoc(collection(db,'todos'),{
+  text:inp,
+  completed:false,
+})
+setInp('')
+}
   return (
     <div className="App h-[100vh] w-[100vw] bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] p-5">
       <div className="container bg-slate-100  max-w-[500px] w-full m-auto rounded-lg p-4 shadow-xl">
       <h1 className="heading text-3xl text-center font-bold p-2">Todo App</h1>
-      <form action="" className='form flex justify-between '>
-        <input type="text" placeholder='Add todo' className='text-input rounded-md p-2 text-xl border w-full'/>
+      <form action="" onSubmit={addTodo} className='form flex justify-between '>
+        <input value={inp} onChange={(e)=>setInp(e.target.value)} type="text" placeholder='Add todo' className='text-input rounded-md p-2 text-xl border w-full'/>
         <button className='plus-button border p-2 ml-2 bg-purple-500 text-slate-100'><AiOutlinePlus size={35} /></button>
       </form>
       <ul className='list'>
@@ -50,7 +61,7 @@ const deleteTodo=async (dummyTodo)=>{
     return <Todo key={index} todo={item} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
   })}
       </ul>
-      <p className='todos-count text-center p-1 font-semibold'>You have <span className='text-green-600 text-xl font-semibold'>2</span> Todos</p>
+      {todos.length>=1 && <p className='todos-count text-center p-1 font-semibold'>You have <span className='text-green-600 text-xl font-semibold'>{todos.length}</span> Todos</p>}
       </div>
       </div>
   )
